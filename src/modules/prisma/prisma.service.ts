deb6ba10383ff9c36775b8c2b4c091abbd085157
Eder-TS <steder20@yahoo.com.br>
2026-04-26 16:53:45 -0300

    Fix: prisma service.

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -1,29 +1,59 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaPg } from '@prisma/adapter-pg';
+// import { Injectable, OnModuleInit } from '@nestjs/common';
+// import { PrismaPg } from '@prisma/adapter-pg';
+// import { PrismaClient } from '@prisma/client';
+// import { Pool } from 'pg';
+
+// @Injectable()
+// export class PrismaService extends PrismaClient implements OnModuleInit {
+//   private pool: Pool;
+//   constructor() {
+//     const pool = new Pool({
+//       connectionString: process.env.DATABASE_URL,
+//     });
+//     super({
+//       adapter: new PrismaPg(pool),
+//     });
+
+//     this.pool = pool;
+//   }
+
+//   async onModuleInit() {
+//     await this.$connect();
+//   }
+
+//   async onApplicationShutdown() {
+//     await this.$disconnect();
+//     // Isso é necessário para os testes e2e.
+//     await this.pool.end();
+//   }
+// }
+
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit {
-  private pool: Pool;
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
-    const pool = new Pool({
-      connectionString: process.env.DATABASE_URL,
-    });
     super({
-      adapter: new PrismaPg(pool),
+      log:
+        process.env.NODE_ENV === 'development'
+          ? ['query', 'error', 'warn']
+          : ['error'],
     });
-
-    this.pool = pool;
   }
 
   async onModuleInit() {
+    if (!process.env.DATABASE_URL) {
+      throw new Error('DATABASE_URL não definida');
+    }
+
     await this.$connect();
   }
 
-  async onApplicationShutdown() {
+  async onModuleDestroy() {
     await this.$disconnect();
-    // Isso é necessário para os testes e2e.
-    await this.pool.end();
   }
 }
