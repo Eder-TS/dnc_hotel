d9e9d75700973da620af5b46824156f4b70acf80
Eder-TS <steder20@yahoo.com.br>
2026-04-26 18:17:00 -0300

    Fix: prisma + adapter.

--- src/modules/prisma/prisma.service.ts
+++ src/modules/prisma/prisma.service.ts
@@ -1,59 +1,76 @@
-// import { Injectable, OnModuleInit } from '@nestjs/common';
-// import { PrismaPg } from '@prisma/adapter-pg';
-// import { PrismaClient } from '@prisma/client';
-// import { Pool } from 'pg';
-
-// @Injectable()
-// export class PrismaService extends PrismaClient implements OnModuleInit {
-//   private pool: Pool;
-//   constructor() {
-//     const pool = new Pool({
-//       connectionString: process.env.DATABASE_URL,
-//     });
-//     super({
-//       adapter: new PrismaPg(pool),
-//     });
-
-//     this.pool = pool;
-//   }
-
-//   async onModuleInit() {
-//     await this.$connect();
-//   }
-
-//   async onApplicationShutdown() {
-//     await this.$disconnect();
-//     // Isso é necessário para os testes e2e.
-//     await this.pool.end();
-//   }
-// }
-
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import { Pool } from 'pg';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  private pool: Pool;
+
   constructor() {
+    const connectionString = process.env.DATABASE_URL;
+
+    if (!connectionString) {
+      throw new Error('DATABASE_URL não definida');
+    }
+
+    const pool = new Pool({
+      connectionString,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    });
+
     super({
+      adapter: new PrismaPg(pool),
       log:
         process.env.NODE_ENV === 'development'
           ? ['query', 'error', 'warn']
           : ['error'],
     });
+
+    this.pool = pool;
   }
 
   async onModuleInit() {
-    if (!process.env.DATABASE_URL) {
-      throw new Error('DATABASE_URL não definida');
-    }
-
     await this.$connect();
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
+    await this.pool.end();
   }
 }
+
+// import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+// import { PrismaClient } from '@prisma/client';
+
+// @Injectable()
+// export class PrismaService
+//   extends PrismaClient
+//   implements OnModuleInit, OnModuleDestroy
+// {
+//   constructor() {
+//     super({
+//       log:
+//         process.env.NODE_ENV === 'development'
+//           ? ['query', 'error', 'warn']
+//           : ['error'],
+//     });
+//   }
+
+//   async onModuleInit() {
+//     if (!process.env.DATABASE_URL) {
+//       throw new Error('DATABASE_URL não definida');
+//     }
+
+//     await this.$connect();
+//   }
+
+//   async onModuleDestroy() {
+//     await this.$disconnect();
+//   }
+// }
